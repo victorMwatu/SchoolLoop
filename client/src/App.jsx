372e67202d4e1317e8bb0360d725582f0efd64d9
victorMwatu <victormwatu@gmail.com>
2025-09-24 04:12:47 +0300

    routes / and /dashboard protected accessed only when user logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,27 @@ import Signup from './components/Signup';
 import './App.css';
 
 function App() {
+  const isLoggedIn = !!localStorage.getItem('token');
+
   return (
     <Router>
       <div className="App">
         <Navigation />
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route
+            path="/"
+            element={
+              isLoggedIn ? <Dashboard /> : <Navigate to="/login" />
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/dashboard"
+            element={
+              isLoggedIn ? <Dashboard /> : <Navigate to="/login" />
+            }
+          />
         </Routes>
       </div>
     </Router>
